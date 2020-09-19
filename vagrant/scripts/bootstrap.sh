fa0c0ccfe11e9f14333af482069319806076240a
Julian Alimin <dmastag@yahoo.com>
2020-09-19 17:58:35 +0700

    perbaiki shell script - pakai folder sendiri - gunakan export untuk bash scriptnya

--- vagrant/scripts/bootstrap.sh
+++ vagrant/scripts/bootstrap.sh
@@ -2,5 +2,6 @@ mkdir /home/vagrant/markdeck
 cd /home/vagrant/markdeck
 curl -LO https://raw.githubusercontent.com/arnehilmann/markdeck/master/markdeck
 chmod a+x markdeck
-echo 'export PATH=/home/vagrant/markdeck:$PATH' >> ~/.bashrc
-cat ~/.bashrc
+if ! grep -q "export.sh" /home/vagrant/.bashrc; then
+    echo "source /vagrant/scripts/export.sh" >> /home/vagrant/.bashrc
+fi
